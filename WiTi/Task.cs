63294bfa26c47c0890fdfc54652bc2f737c0a59e
Arkadiusz_Ziółkowski <aziolkowski3920@gmail.com>
2016-05-17 12:19:14 +0200

    data reader

---
+++ WiTi/Task.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WiTi
+{
+    class Task
+    {
+        public int t; // czas trwania
+        public int p; // współczynnik kary
+        public int f; // wymagany czas zakończenia
+    }
+}
---
+++ WiTi/TaskService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace WiTi
+{
+    class TaskService
+    {
+        public Task[] ReadData(string filename)
+        {
+            string directory = System.IO.Directory.GetCurrentDirectory();
+            string[] lines = System.IO.File.ReadAllLines(directory + "\\" + filename);
+            int tasksCount = int.Parse(lines[0]);
+            Task[] data = new Task[tasksCount + 1];
+
+            for (int i = 1; i < lines.Length; ++i)
+            {
+                Task currentTask = new Task();
+                string[] line = lines[i].Split(' ');
+                currentTask.t = int.Parse(line[0]);
+                currentTask.p = int.Parse(line[1]);
+                currentTask.f = int.Parse(line[2]);
+                data[i] = currentTask;
+            }
+            return data;
+        }
+    }
+}
